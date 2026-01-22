4824f4870d93987181527c81b3955e2ef3bfbf49
MC2 <mitchell.c.cornwell@gmail.com>
2026-01-21 18:46:13 -0600

    New code.

---
+++ src/tabs/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (<ul>
+  <li>Hey</li>
+  <li>there</li>
+  <li>mate</li>
+  </ul>)
+
+  ReactDOM.render(myList, document.getElementById('app'));
